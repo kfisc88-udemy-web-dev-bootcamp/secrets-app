fc75abdd947324bfd5f17b13d8b6f0d3a5d5daa9
Kelley Fischer <krfisc@outlook.com>
2020-10-10 13:07:10 -0500

    Initial set up for app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 //jshint esversion:6
+const express = require("express");
+const bodyParser = require("express");
+const ejs = require("ejs");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.listen(3000, function(req, res){
+    console.log("Server is jogging on port 3000")
+});
