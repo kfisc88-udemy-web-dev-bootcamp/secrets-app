9a7d4836f8c86deb5725098679c17722ef90cd85
Kelley Fischer <krfisc@outlook.com>
2020-10-10 13:12:38 -0500

    add get responses

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get("/", function(req, res){
+    res.render("home");
+});
 
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.get("/login", function(req, res){
+    res.render("login");
+});
 
 app.listen(3000, function(req, res){
     console.log("Server is jogging on port 3000")
