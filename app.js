6720be32fdc87179fee121a501a62d18ce775bc1
Kelley Fischer <krfisc@outlook.com>
2020-10-12 11:54:28 -0500

    Add passportjs authentication

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const passportLocalMongoose = require("passport-local-mongoose");
 
 // Set up express
 const app = express();
+
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -21,30 +22,32 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(session({
     secret: process.env.SECRET,
     resave: false,
-    saveUninitialized: true
+    saveUninitialized: false
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Set up mongo database
 mongoose.connect("mongodb://localhost:27017/secretDB", {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+    useUnifiedTopology: true
 });
-
+mongoose.set("useCreateIndex", true);
 
 // Schema for new users
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
+
 userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
 
 // Session Strategy and Serialization
 passport.use(User.createStrategy());
- 
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -54,24 +57,26 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
+app.get("/login", function(req, res){
+    res.render("login");
+});
+
 app.get("/register", function(req, res){
     res.render("register");
 });
 
 app.get("/secrets", function(req, res){
     if (req.isAuthenticated()) {
-        console.log("User authenticated.");
         res.render("secrets");
     } else {
-        console.log("Access denied, please log in.");
         res.redirect("/login");
     }
 });
 
-app.get("/login", function(req, res){
-    res.render("login");
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
 });
-
 
 // __________________________POST__________________________  \\
 
@@ -89,14 +94,16 @@ app.post("/register", function(req ,res){
 });
 
 app.post("/login", function(req, res){
+    
     const user = new User({
-        email: req.body.username,
+        username: req.body.username,
         password: req.body.password
     });
 
-    req.login(user, function(err){
-        if(err){
+    req.login(user, function(err) {
+        if (err) {
             console.log(err);
+            res.redirect("/login");
         } else {
             passport.authenticate("local")(req, res, function(){
                 res.redirect("/secrets");
